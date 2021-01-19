4a9ce6afe9923edfa75f2b068e9037c63765ca06
Gergely Koncz <gergo.koncz@gmail.com>
2021-01-19 18:22:57 +0100

    Nuget lesson

---
+++ Nuget/Program.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Nuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = Path.Combine(AppContext.BaseDirectory, "employees.csv");
+            using (var streamReader = new StreamReader(file))
+            {
+                var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+
+                while (csvReader.Read())
+                {
+                    string name = csvReader.GetField<string>(0);
+                    string salary = csvReader.GetField<string>(1);
+                    string department = csvReader.GetField<string>(2);
+                    
+                    Console.WriteLine($"{name}\t{salary}\t{department}");
+                }
+            }
+        }
+    }
+}
