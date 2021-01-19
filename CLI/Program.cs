3fd2f553765128360d25517472423ae8276090d2
Gergely Koncz <gergo.koncz@gmail.com>
2021-01-19 18:10:51 +0100

    CLI lession

---
+++ CLI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Missing parameters.");
+                return;
+            }
+
+            string fileName = args[0];
+
+            using (var writer = File.CreateText(fileName))
+            {
+                writer.WriteLine("Hello world");
+            }
+
+            Console.WriteLine($"Successfully created file: {fileName}");
+        }
+    }
+}
