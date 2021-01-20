fe5d0677549c194ee51ff313d5816fa2ae7ab55e
Gergely Koncz <gergo.koncz@gmail.com>
2021-01-20 15:59:52 +0100

    ASPNET Identity demo

---
+++ IdentityDemo/IdentityDemo/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace IdentityDemo.Controllers
+{
+    public class UserDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> RegisterUser(UserDto userDto)
+        {
+            var user = new IdentityUser(userDto.UserName);
+            await userManager.CreateAsync(user, userDto.Password);
+
+            return this.Ok(userDto);
+        }
+    }
+}
